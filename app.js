6dec6dda40d2a1cd96d55f33d577a193e768ff1d
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 18:44:37 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+          const express = require('express');
+          const path = require('path');
 
-  const bodyParser = require('body-parser');
+            const bodyParser = require('body-parser');
 
-const cors = require('cors');
+          const cors = require('cors');
 
 
-const passport = require('passport');
+  const passport = require('passport');
 
- const mongoose = require('mongoose');
+   const mongoose = require('mongoose');
 
   const config = require('./config/database');
 
@@ -18,48 +18,48 @@ const passport = require('passport');
 
   mongoose.connect(config.database, { useMongoClient: true});
 
-mongoose.connection.on('connected', () => {
-  console.log('Connected to Database '+config.database);
-});
+      mongoose.connection.on('connected', () => {
+        console.log('Connected to Database '+config.database);
+      });
 
-mongoose.connection.on('error', (err) => {
-  console.log('Database error '+err);
-});
+      mongoose.connection.on('error', (err) => {
+        console.log('Database error '+err);
+      });
 
-const app = express();
+      const app = express();
 
-const users = require('./routes/users');
-
-
- const port = process.env.PORT || 3000;
+    const users = require('./routes/users');
 
 
-app.use(cors());
+     const port = process.env.PORT || 3000;
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(cors());
 
 
-app.use(bodyParser.json());
+      app.use(express.static(path.join(__dirname, 'public')));
 
 
- app.use(passport.initialize());
-  app.use(passport.session());
-
-require('./config/passport')(passport);
-
-app.use('/users', users);
+      app.use(bodyParser.json());
 
 
-app.get('/', (req, res) => {
-  res.send('invaild endpoint');
-});
+       app.use(passport.initialize());
+        app.use(passport.session());
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+      require('./config/passport')(passport);
+
+      app.use('/users', users);
 
 
-app.listen(port, () => {
-  console.log('Server started on port '+port);
-});
+                app.get('/', (req, res) => {
+                  res.send('invaild endpoint');
+                });
+
+                app.get('*', (req, res) => {
+                  res.sendFile(path.join(__dirname, 'public/index.html'));
+                });
+
+
+                app.listen(port, () => {
+                  console.log('Server started on port '+port);
+                });
