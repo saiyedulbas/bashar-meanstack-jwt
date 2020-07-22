944f071c819c4524e83be7ef00f1a6673461ade4
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 18:37:45 +0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // configure the code below with your username, password and mlab database information
-  database: 'mongodb+srv://bashar:7XTV55KW@cluster0-5zahv.mongodb.net/meanauthapp?retryWrites=true&w=majority',   //prod
-  //database: 'mongodb://localhost:27017/meanauth',    //dev
-  secret: 'yoursecret'
+  
+  database: 'mongodb+srv://bashar:7XTV55KW@cluster0-5zahv.mongodb.net/meanauthapp?retryWrites=true&w=majority', 
+  
+  secret: 'bashar24'
 }
