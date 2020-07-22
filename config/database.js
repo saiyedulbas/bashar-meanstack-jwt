3c1767e03f67890eb644b01f946345270f124535
Saiyedul Bashar <saiyedulbas30@gmail.com>
2020-07-22 15:33:39 +0600

    First Commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb+srv://bashar:7XTV55KW@cluster0-5zahv.mongodb.net/meanauthapp?retryWrites=true&w=majority',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
