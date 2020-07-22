ea08cb8ef97fb572aa4d8ec87d3050b7ef3c143c
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 18:30:17 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,25 +6,16 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
-// Connect To Database (NEW) But not working!!!!!!!!!! (because of secret in db.js!!!!!)
-//const db = require('./config/database');
-// Map global promise - get rid of warning
-//mongoose.Promise = global.Promise;
-// Connect to mongoose
-//mongoose.connect(db.mongoURI, {
-    //useMongoClient: true
-//})
-//.then(() => console.log('MongoDB Connected...'))
-//.catch(err => console.log(err));
 
 
-// Connect To Database (OLD CODE)
+
+
 mongoose.connect(config.database, { useMongoClient: true});
-// On Connection
+
 mongoose.connection.on('connected', () => {
   console.log('Connected to Database '+config.database);
 });
-// On Error
+
 mongoose.connection.on('error', (err) => {
   console.log('Database error '+err);
 });
@@ -33,19 +24,19 @@ const app = express();
 
 const users = require('./routes/users');
 
-// Port Number
+
 const port = process.env.PORT || 3000;
 
-// CORS Middleware
+
 app.use(cors());
 
-// Set Static Folder
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Body Parser Middleware
+
 app.use(bodyParser.json());
 
-// Passport Middleware
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -53,7 +44,7 @@ require('./config/passport')(passport);
 
 app.use('/users', users);
 
-// Index Route
+
 app.get('/', (req, res) => {
   res.send('invaild endpoint');
 });
@@ -62,7 +53,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// Start Server
+
 app.listen(port, () => {
   console.log('Server started on port '+port);
 });
