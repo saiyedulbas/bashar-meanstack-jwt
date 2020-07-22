e918172ed6fe02481bd75080019a1f7027ef1fab
Saiyedul Bashar <65618180+saiyedulbas@users.noreply.github.com>
2020-07-22 18:43:42 +0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
-module.exports = {
-  
-  database: 'mongodb+srv://bashar:7XTV55KW@cluster0-5zahv.mongodb.net/meanauthapp?retryWrites=true&w=majority', 
-  
-  secret: 'bashar24'
-}
+            module.exports = {
+
+                      database: 'mongodb+srv://bashar:7XTV55KW@cluster0-5zahv.mongodb.net/meanauthapp?retryWrites=true&w=majority', 
+
+                  secret: 'bashar24'
+            }
